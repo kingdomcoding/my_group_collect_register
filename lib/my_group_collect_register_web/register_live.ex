a996b2cb948caafbaba175016b3ce9c56e33d7fc
Oreoluwa James <kingdomcoding@gmail.com>
2024-10-18 09:50:41 +0100

    feat: create register live

---
+++ lib/my_group_collect_register_web/register_live.ex
@@ -0,0 +1,9 @@
+defmodule MyGroupCollectRegisterWeb.RegisterLive do
+  use MyGroupCollectRegisterWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    <h1>Register Live</h1>
+    """
+  end
+end
--- lib/my_group_collect_register_web/router.ex
+++ lib/my_group_collect_register_web/router.ex
@@ -18,6 +18,7 @@ defmodule MyGroupCollectRegisterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/register", RegisterLive
   end
 
   # Other scopes may use custom stacks.
