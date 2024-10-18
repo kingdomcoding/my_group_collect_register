155f8d21c77f6a213c601fbc3b3c50db7fa1f45c
Oreoluwa James <kingdomcoding@gmail.com>
2024-10-18 11:02:16 +0100

    feat: add agree to terms attribute to fields

--- lib/my_group_collect_register_web/register_live/page_1_form_fields.ex
+++ lib/my_group_collect_register_web/register_live/page_1_form_fields.ex
@@ -4,7 +4,19 @@ defmodule MyGroupCollectRegisterWeb.RegisterLive.Page1FormFields do
   attributes do
     attribute :email, :string, public?: true, allow_nil?: false
     attribute :password, :string, public?: true, allow_nil?: false, constraints: [min_length: 8]
+    attribute :agree_to_terms?, :boolean, public?: true, allow_nil?: false
   end
+
+  validations do
+    validate fn changeset, _context ->
+      if Ash.Changeset.get_attribute(changeset, :agree_to_terms?) do
+        :ok
+      else
+        {:error, field: :agree_to_terms?, message: "must agree to continue"}
+      end
+    end
+  end
+
 
   actions do
     create :submit
