cdf53e99f46a9ea3749d8ae624e1a3c80c463e23
Oreoluwa James <kingdomcoding@gmail.com>
2024-10-18 11:04:01 +0100

    feat: connect form to fields

--- lib/my_group_collect_register_web/register_live/page_1_form.ex
+++ lib/my_group_collect_register_web/register_live/page_1_form.ex
@@ -10,6 +10,8 @@ defmodule MyGroupCollectRegisterWeb.RegisterLive.Page1Form do
   end
 
   def render(assigns) do
+    # TODO: Add url linking
+    # <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms of Service</a>
     ~H"""
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -18,37 +20,40 @@ defmodule MyGroupCollectRegisterWeb.RegisterLive.Page1Form do
                   <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                       Create an account
                   </h1>
-                  <form class="space-y-4 md:space-y-6" action="#">
-                      <div>
-                          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                          <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
-                      </div>
-                      <div>
-                          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                      </div>
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="terms" class="font-light text-gray-500 dark:text-gray-300">
-                              I agree to the
-                                <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms of Service</a>
-                              and
-                                <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Privacy Policy</a>
-                            </label>
-                          </div>
-                      </div>
-                      <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                          Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                      </p>
-                  </form>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                  </p>
+                  <.simple_form for={@form} class="space-y-4 md:space-y-6" phx-change="validate" phx-submit="submit" phx-target={@myself}>
+                      <.input field={@form[:email]} label="Email"/>
+                      <.input field={@form[:password]} label="Password" type="password"/>
+                      <.input field={@form[:agree_to_terms?]} label="I agree to the Terms of Service and Privacy Policy" type="checkbox"/>
+
+                      <:actions>
+                        <.button>Create account</.button>
+                      </:actions>
+                  </.simple_form>
               </div>
           </div>
       </div>
     </section>
     """
+  end
+
+  def handle_event("validate", params, socket) do
+    form_params = Map.get(params, "form", %{})
+    form = AshPhoenix.Form.validate(socket.assigns.form, form_params)
+    {:noreply, assign(socket, :form, form)}
+  end
+
+  def handle_event("submit", params, socket) do
+    form_params = Map.get(params, "form", %{})
+
+    case AshPhoenix.Form.submit(socket.assigns.form, params: form_params) do
+      {:ok, _form_struct} ->
+        # TODO: Navigate to next page
+        {:noreply, socket}
+      {:error, form_with_error} ->
+        {:noreply, assign(socket, :form, form_with_error)}
+    end
   end
 end
